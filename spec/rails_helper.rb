cd1c9eb0a7caddc3b33bc7a6a96ad011114aba23
Artur Beljajev <artur.beljajev@internet.ee>
2016-10-01 01:01:37 +0300

    include time helpers in spec helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -58,6 +58,8 @@ RSpec.configure do |config|
   config.filter_run focus: true
   config.run_all_when_everything_filtered = true
 
+  config.include ActiveSupport::Testing::TimeHelpers
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
