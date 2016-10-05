253f8fc67b0cc4c878029de674f588e117d85687
Artur Beljajev <artur.beljajev@internet.ee>
2016-10-05 11:10:50 +0300

    improve development env config

--- config/environments/development-example.rb
+++ config/environments/development-example.rb
@@ -16,6 +16,8 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  config.logger = ActiveSupport::Logger.new(nil)
+
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
@@ -30,10 +32,7 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
 
   # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
-
-  # config.log_tags = [:subdomain, :uuid, :remote_ip]
-  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new(ENV['app_name'] || 'registry'))
+  config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'localhost:8081' }
